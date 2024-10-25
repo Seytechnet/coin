b63f36ee0f61438485e26ecebbd4258f47d18446
Seytech <107435860+Seytechnet@users.noreply.github.com>
2024-10-25 18:02:17 +0100

    ip

--- Main/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('wallets', views.wallets, name='wallets'),
-    path('importkey', views.importkey , name='importkey'),
-    path('reference/', views.reference, name='reference'),
-]
